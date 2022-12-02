869fbfaa341ed75b738ac2c2aa6d8fa72b3c9a58
JDM Digital <nerds@jdmdigital.co>
2022-12-02 12:43:59 -0600

    Updates

--- hat-tip.js
+++ hat-tip.js
@@ -1,39 +1,37 @@
 // Hat-Tip v 1.0
 // https://github.com/jdmdigital/hat-tip
-jQuery(document).ready(function(){
-	function hattip_die(){
-		$('#hattip').removeClass('fire');
+function hattip_die(){
+	$('#hattip').removeClass('fire');
 
-		setTimeout(function() {
-			$('#hattip #msg').html('');
-			$('#hattip').removeAttr('class');
-		},400);
-	}
+	setTimeout(function() {
+		$('#hattip #msg').html('');
+		$('#hattip').removeAttr('class');
+	},400);
+}
 
-	function hattip_fire(msg = 'Hello World!',colorClass = 'ht-default'){
-		var hhwaiting = 50;
+function hattip_fire(msg = 'Hello World!',colorClass = 'ht-default'){
+	var hhwaiting = 50;
 
-		if($('#hattip').hasClass('fire')) {
-			hattip_die();
-			hhwaiting = 500;
-		} 			   
-
-		setTimeout(function() {
-			$('#hattip #msg').html(msg);
-			$('#hattip').addClass(colorClass).addClass('display'); 
-		setTimeout(function() { 
-			$('#hattip').addClass('fire');
-		}, 25);
-
-		// Auto-hide after 6 seconds if no other notifications are added?
-		/*setTimeout(function() {
+	if($('#hattip').hasClass('fire')) {
 		hattip_die();
-		},6000);*/
+		hhwaiting = 500;
+	} 			   
 
-		},hhwaiting);
-	}
-	
-	$('#hattip button').click(function() {
-		hattip_die();
-	});
+	setTimeout(function() {
+		$('#hattip #msg').html(msg);
+		$('#hattip').addClass(colorClass).addClass('display'); 
+	setTimeout(function() { 
+		$('#hattip').addClass('fire');
+	}, 25);
+
+	// Auto-hide after 6 seconds if no other notifications are added?
+	/*setTimeout(function() {
+	hattip_die();
+	},6000);*/
+
+	},hhwaiting);
+}
+
+jQuery('#hattip button').click(function() {
+	hattip_die();
 });
